2eb811a808bba067333891a0250bef47f062c3e6
amanm6925 <amanm6925@gmail.com>
2019-04-07 21:27:43 +0530

    incomplete

--- app/src/main/java/com/iitg/interaction/facultystudentinteractionportal/TimeTable.java
+++ app/src/main/java/com/iitg/interaction/facultystudentinteractionportal/TimeTable.java
@@ -1,11 +1,32 @@
 package com.iitg.interaction.facultystudentinteractionportal;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class TimeTable extends Activity {
+
+
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timetable);
+
+
+    }
+
+    public void addclick(View v){
+
+        mDatabase.child("tasks").child("1").setValue("take it");
+        Button b1 = findViewById(R.id.addbutton);
+        b1.setBackgroundColor(Color.parseColor("red"));
+
     }
 }
