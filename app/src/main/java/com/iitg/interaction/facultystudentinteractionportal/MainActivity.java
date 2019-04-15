0ca9d28330077bca2309bf1e491b8b1125483a3a
amanm6925 <amanm6925@gmail.com>
2019-04-15 14:44:22 +0530

    Created Search with a bug of researching

--- app/src/main/java/com/iitg/interaction/facultystudentinteractionportal/MainActivity.java
+++ app/src/main/java/com/iitg/interaction/facultystudentinteractionportal/MainActivity.java
@@ -75,6 +75,13 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    public void SearchClick(View v) {
+        if(v.getId() == R.id.search_button){
+            Intent i = new Intent(MainActivity.this , Search.class );
+            startActivity(i);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/iitg/interaction/facultystudentinteractionportal/Search.java
@@ -0,0 +1,126 @@
+package com.iitg.interaction.facultystudentinteractionportal;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class Search extends AppCompatActivity {
+
+
+
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    ArrayList<String> al = new ArrayList<String>();
+    ListView r;
+    TextView t;
+    Activity truth = this;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+
+
+        Button go = (Button) findViewById(R.id.Go);
+        t = (TextView) findViewById(R.id.srch);
+
+        r = (ListView) findViewById(R.id.table);
+
+
+        /*go.setOnClickListener(new View.OnClickListener() {
+
+
+
+        });*/
+
+        //ad.notifyDataSetChanged();
+    }
+
+
+
+    public void GoClick (View view){
+
+
+
+            r = (ListView) findViewById(R.id.table);
+            al.clear();
+            final ArrayAdapter<String>  ad = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+            r.setAdapter(ad);
+            ad.notifyDataSetChanged();
+
+            DatabaseReference us = db.getReference().child("Courses");
+
+            us.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    /**/
+                    String base = dataSnapshot.getValue().toString();
+                    String[] split =  base.split(",");
+                    int k = 0;
+                    for(k = 0; k< split.length; k++){
+                        if(split[k].contains("courseID")){
+                            String ans = split[k].substring(10, split[k].length());
+                            String prev = t.getText().toString();
+                            if(ans.toLowerCase().contains(prev.toLowerCase())){
+                                al.add(ans);
+                            }
+                            /*String prev = t.getText().toString();
+                            t.setText(prev + ans);*/
+
+
+                        }
+                    }
+
+                    hideKeyboard(truth);
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        ad.notifyDataSetChanged();
+
+
+
+
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+
+
+}
