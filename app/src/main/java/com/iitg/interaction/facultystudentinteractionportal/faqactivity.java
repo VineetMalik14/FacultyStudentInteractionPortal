27c2fc0873da73db009480746454bb5dce502c7e
Annanya Pratap <annanyapr@gmail.com>
2019-04-20 22:32:29 +0530

    FAQ added

--- app/src/main/java/com/iitg/interaction/facultystudentinteractionportal/faqactivity.java
+++ app/src/main/java/com/iitg/interaction/facultystudentinteractionportal/faqactivity.java
@@ -11,10 +11,16 @@ public class faqactivity extends AppCompatActivity {
   Button faq1;
   Button faq2;
   Button faq3;
-  TextView ans1;
+    Button faq4;
+    Button faq5;
+
+    TextView ans1;
   TextView ans2;
   TextView ans3;
-  public  int c1,c2,c3;
+  TextView ans4;
+  TextView ans5;
+
+  public  int c1,c2,c3,c4,c5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +30,24 @@ public class faqactivity extends AppCompatActivity {
         faq1=findViewById(R.id.faq1);
         faq2=findViewById(R.id.faq2);
         faq3=findViewById(R.id.faq3);
+        faq4=findViewById(R.id.faq4);
+        faq5=findViewById(R.id.faq5);
         ans1=findViewById(R.id.ans1);
         ans2=findViewById(R.id.ans2);
         ans3=findViewById(R.id.ans3);
-
+        ans4=findViewById(R.id.ans4);
+        ans5=findViewById(R.id.ans5);
 
         ans1.setVisibility(View.GONE);
         ans2.setVisibility(View.GONE);
         ans3.setVisibility(View.GONE);
+        ans4.setVisibility(View.GONE);
+        ans5.setVisibility(View.GONE);
+
         c1=0;
          c2=0;
          c3=0;
+         c4=0;c5=0;
          if(UserInfo.usertype.equals("Prof"))
          {
              faq1.setText("Q. How To Add New Course?");
@@ -71,6 +84,28 @@ public class faqactivity extends AppCompatActivity {
         {
             faq3.setText("Q. How To post query in course");
             ans3.setText("login->navbar->cources->mycourses->discusion forum->click add bottom right corner ");
+        }
+        if(UserInfo.usertype.equals("Prof"))
+        {
+            faq4.setText("Q How to enroll to course?");
+            ans4.setText("Prof cannot enroll to a course. ");
+        }
+
+        else
+        {
+            faq4.setText("Q. How to enroll to course?");
+            ans4.setText("login->navbar->cources->allcourses->search for your course->click on course-> click on enroll button->enter key ");
+        }
+        if(UserInfo.usertype.equals("Prof"))
+        {
+            faq5.setText("Q How to delete a course?");
+            ans5.setText("login->click on menu bar on top right -> select the course to delete ");
+        }
+
+        else
+        {
+            faq5.setText("Q.  How to delete a course?");
+            ans5.setText("Student cannot delete a course. ");
         }
 
         faq1.setOnClickListener(new View.OnClickListener() {
@@ -115,6 +150,36 @@ public class faqactivity extends AppCompatActivity {
                 {
                     ans3.setVisibility(View.GONE);
                     c3++;
+                }
+            }
+        });
+        faq4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(c4%2==0)
+                {
+                    ans4.setVisibility(View.VISIBLE);
+                    c4++;
+                }
+                else
+                {
+                    ans4.setVisibility(View.GONE);
+                    c4++;
+                }
+            }
+        });
+        faq5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(c5%2==0)
+                {
+                    ans5.setVisibility(View.VISIBLE);
+                    c5++;
+                }
+                else
+                {
+                    ans5.setVisibility(View.GONE);
+                    c5++;
                 }
             }
         });
